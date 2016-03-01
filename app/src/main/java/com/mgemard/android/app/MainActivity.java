367aa99ec8057bfc7f45ae894d76f4a9c669e3b5
unknown <admin@NWS-JMO.Euronav.local>
2016-03-01 23:52:39 +0100

    Initial commit

---
+++ app/src/main/java/com/mgemard/android/app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mgemard.android.app;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+}
